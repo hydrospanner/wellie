534136a3d77200bbd702a262d82ac060cc0664d7
hydrospanner <johntuckerusa@gmail.com>
2018-09-23 06:17:58 -0500

    add well detail page. index page lists wells linking to detail page.

--- wellie/wells/views.py
+++ wellie/wells/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Well
+
+
 def index(request):
-    context = {}
+    wells = Well.objects.all()
+    context = {'wells': wells}
     return render(request, 'wells/index.html', context)
+
+def get_child_fields_as_dict(fields, well):
+    fields = {'well': ['track']}
+    pass
+
+def well_detail(request, pk):
+    well = Well.objects.get(pk=pk)
+    well_dict = well.__dict__
+    print(well_dict)
+    tracks = well.track_set.all()
+    print([track.__dict__ for track in tracks])
+    context = {'well': well}
+    return render(request, 'wells/well_detail.html', context)
